60bc92c7a7458f6ff4231e3019a29452d9c26973
shivam shandilya <shandilyapro@gmail.com>
2020-08-05 19:26:03 +0530

    initial build

---
+++ contracts/Buy.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+contract Buy{
+
+    address[16] public buyers;
+
+    function buy(uint suppID) public returns (uint) {
+        require(suppID>0 && suppID<=15);
+        buyers[suppID]=msg.sender;
+        return suppID;
+    }
+
+    function getBuyers() public view returns (address[16] memory) {
+        return buyers;
+    }
+
+}
