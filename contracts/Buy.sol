8d1d6b1e8433cc36cae6babf2c533699a90e5a64
shivam shandilya <shandilyapro@gmail.com>
2020-09-05 09:43:34 +0530

    added functionality

--- contracts/Buy.sol
+++ contracts/Buy.sol
@@ -1,17 +1,41 @@
 pragma solidity ^0.5.0;
 
+
 contract Buy{
+    struct buyer{
+        address buyer_hash;
+        string buyer_name;
+        string contact_number;
+        uint buyer_age;
+    }
+    
+    buyer[16] public buyers;
 
-    address[16] public buyers;
-
-    function buy(uint suppID) public returns (uint) {
+    function buy(uint suppID,string memory name,string memory contact,uint age) public returns (uint) {
         require(suppID>0 && suppID<=15);
-        buyers[suppID]=msg.sender;
+        buyer memory new_buyer=buyer();
+        new_buyer.buyer_name=name;
+        new_buyer.buyer_age=age;
+        new_buyer.contact_number=contact;
+        new_buyer.buyer_hash=msg.sender;
+        buyers[suppID]=new_buyer;
         return suppID;
     }
 
-    function getBuyers() public view returns (address[16] memory) {
+    function getBuyers() public view returns (buyer[16] memory) {
         return buyers;
+    }
+    
+    function edit_name(uint suppID,string memory new_name) public{
+        buyers[suppID].buyer_name=new_name;
+    }
+    
+    function edit_age(uint suppID,uint new_age) public{
+        buyers[suppID].buyer_age=new_age;
+    }
+    
+    function edit_contact(uint suppID,string memory new_contact) public{
+        buyers[suppID].contact_number=new_contact;
     }
 
 }
--- contracts/Sell.sol
+++ contracts/Sell.sol
@@ -1,21 +1,41 @@
 pragma solidity ^0.5.0;
 
+
 contract Sell{
-
-    address[16] public sellers;
-
-    constructor() public{
-        sellers[1]=address(234);
+    struct buyer{
+        address seller_hash;
+        string seller_name;
+        string contact_number;
+        uint seller_age;
     }
+    
+    seller[16] public sellers;
 
-    function sell(uint suppID) public returns (uint) {
+    function buy(uint suppID,string memory name,string memory contact,uint age) public returns (uint) {
         require(suppID>0 && suppID<=15);
-        sellers[suppID]=msg.sender;
+        seller memory new_seller=seller();
+        new_seller.seller_name=name;
+        new_seller.seller_age=age;
+        new_seller.contact_number=contact;
+        new_seller.seller_hash=msg.sender;
+        sellers[suppID]=new_seller;
         return suppID;
     }
 
-    function getSellers() public view returns (address[16] memory) {
+    function getSellers() public view returns (seller[16] memory) {
         return sellers;
+    }
+
+    function edit_name(uint suppID,string memory new_name) public{
+        sellers[suppID].seller_name=new_name;
+    }
+    
+    function edit_age(uint suppID,uint new_age) public{
+        sellers[suppID].seller_age=new_age;
+    }
+    
+    function edit_contact(uint suppID,string memory new_contact) public{
+        sellers[suppID].contact_number=new_contact;
     }
 
 }
